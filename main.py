c5d6eb3ff0767a70019a918e21f9849094c3166b
serbimolot <serb979@gmail.com>
2021-12-13 17:28:12 +0200

    pytest implementation

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import sys
-import traceback
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from assert_custom import *
-from selenium_ext import *
 from tests import *
+import pytest
 
+def test_first_in_search_test(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    searchTest(selenium,'kiwitcms/Kiwi','kiwitcms/Kiwi')
 
-s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
-
-try:
-    searchTest(drive0r,'kiwitcms/Kiwi','kiwitcms/Kiwi')
-    searchTest(driver,'Kiwi','kiwitcms/Kiwi', True)
-    SignUpFieldsTest(driver, 'testingemail@gmail.com', "asdf1234", "Wtawwwse", "n")
-    SignUpFieldsTest(driver, 'testingem**ail@gmail.com', "asdf1234", "Wtawwwse", "n")
-    SignUpFieldsTest(driver, 'testingem**ail@gmail.com', "asdf1234", "і", "n")
-    SignUpFieldsTest(driver, 'testingemail@gmail.com', "asdf123іівц4", "і1414124222", "n")
-except AssertFailed:
-    print("Tests failed")
+def test_not_first_in_search_test(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    searchTest(selenium,'Kiwi','kiwitcms/Kiwi', True)
+        
+def test_signup_easy_pass(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    try:
+        SignUpFieldsTest(selenium, 'testingemail@gmail.com', "asdf1234", "Wtawwwse", "n", "password")
+    except:
+        assert True  == True
+def test_signup_without_num(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    try:
+        SignUpFieldsTest(selenium, 'testinsdcgemail@gmail.com', "CwaCsaCdfgsetr", "Wtawwwse", "n", "password")
+    except:
+        assert True  == True
+def test_signup_wring_email(selenium):
     
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    try:
+        SignUpFieldsTest(selenium, 'testingem**ail@gmail.com', "CwaCsaCdfgse2415tr", "Wtawwwse", "n", "email")
+    except:
+        assert True  == True
 
-driver.quit()
+def test_signup_short_password(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    try:
+        SignUpFieldsTest(selenium, 'testingemail@gmail.com', "asdf4", "Wtawwwse", "n", "password")
+    except:
+        assert True  == True
+def test_signup_wrong_password_2(selenium):
+    selenium.get("https://github.com/")
+    selenium.maximize_window()
+    try:
+        SignUpFieldsTest(selenium, 'testingemail@gmail.com', "asdf123іівц4", "і1414124222", "n", "password")
+
+    except:
+        assert True  == True
--- selenium_ext.py
+++ selenium_ext.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import selenium
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -8,22 +9,40 @@ import traceback
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from assert_custom import *
 from selenium_ext import *
+import pytest
 
-def CSS_ClassCBtnlick( driver, css_selector, action_name):
+MAX_ELEMENT_WAIT_TIME = 10
+
+def CSS_ClassCBtnlick( driver, css_selector, action_name, expect=False):
     try:
-        button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
-        button.click()   
-    except TimeoutException:
+        if(expect==True):
+            with pytest.raises(selenium.common.exceptions.TimeoutException) as e_info:
+                button = WebDriverWait(driver, MAX_ELEMENT_WAIT_TIME).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+                button.click()
+        else:    
+            button = WebDriverWait(driver, MAX_ELEMENT_WAIT_TIME).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+            button.click()
+    except selenium.common.exceptions.TimeoutException:
         print(action_name + " failed")
         return False
+    finally:
+        print(action_name + " passed")
+        return True
+
     print(action_name + " passed")
     return True
 
-def Send_Keys_On_Visibility( driver, waitTime, by, msg, selector):
-    emailField = WebDriverWait(driver, waitTime).until(EC.visibility_of_element_located((by, selector)))
-    emailField.clear()
-    emailField.send_keys(msg)
+def Send_Keys_On_Visibility( driver, by, msg, selector,expect = False):
+    if(expect==True):
+        with pytest.raises(selenium.common.exceptions.TimeoutException) as e_info:
+            emailField = WebDriverWait(driver, MAX_ELEMENT_WAIT_TIME).until(EC.visibility_of_element_located((by, selector)))
+            emailField.clear()
+            emailField.send_keys(msg)
+    else:    
+        emailField = WebDriverWait(driver, MAX_ELEMENT_WAIT_TIME).until(EC.visibility_of_element_located((by, selector)))
+        emailField.clear()
+        emailField.send_keys(msg)
+
 
 
